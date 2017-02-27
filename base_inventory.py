4182d1d5cd887f1681bb9dc48f0e99d00f1e4a3d
plzhang <plzhang2013@gmail.com>
2017-02-27 14:08:40 -0500

    added dict_inventory sub class

--- base_inventory.py
+++ base_inventory.py
@@ -3,6 +3,9 @@
 """
 class BaseInventory:
 
+  def __init__(self):
+    pass
+
   def get_product(self, product_id):
     """ Get a product info with id
 
@@ -10,7 +13,7 @@ class BaseInventory:
       product_id(int): the id of the product to be retrieved
 
     Returns:
-      the product info in dict format
+      the product info
     """
     pass
 
@@ -23,13 +26,13 @@ class BaseInventory:
     pass
 
 
-  def put_product(self, product_id, product_info):
+  def put_product(self, id, info):
     """ Put the product in the inventory
         The product can either exists in the inventory or not.
 
     Args:
-      product_id(int): the id of the product
-      product_info(dict): the info of the product
+      id(int): the id of the product
+      info(dict): the info of the product
 
     Returns:
       If put success or not.
