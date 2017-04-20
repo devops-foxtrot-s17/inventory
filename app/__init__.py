ff07d8bdcfd8af1c04d08a2cad9b5ece9fe3607c
plzhang <plzhang2013@gmail.com>
2017-04-20 12:18:02 -0400

    init flasgger code.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import logging
 
 from flask import Flask
+from flasgger import Swagger
 
 # Create the Flask aoo
 app = Flask(__name__)
@@ -8,6 +9,21 @@ app = Flask(__name__)
 # Load Configurations
 # app.config.from_object('config')
 app.config['LOGGING_LEVEL'] = logging.INFO
+app.config['SWAGGER'] = {
+    "swagger_version": "2.0",
+    "specs": [
+        {
+            "version": "1.0.0",
+            "title": "Inventory ",
+            "description": "This is the inventory api.",
+            "endpoint": 'v1_spec',
+            "route": '/v1/spec'
+        }
+    ]
+}
+
+# Initialize Swagger after configuring it
+Swagger(app)
 
 import server
 import utils
